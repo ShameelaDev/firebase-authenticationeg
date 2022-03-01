61211439dec88c05b00c644e5f4167315a3fce76
ShameelaDev <shemeelamoidheen@gmail.com>
2022-03-01 11:33:57 +0530

    authentication repository file created

---
+++ lib/src/app.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_authentication/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
---
+++ lib/src/bloc/authentication/authentication_repository.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepository{
+
+  registerWitheEmailPassword(String email, String password)async{
+    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+  return userCredential.user;
+  }
+
+  loginWitheEmailPassword(String email, String password)async{
+    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+    return userCredential.user;
+  }
+}
