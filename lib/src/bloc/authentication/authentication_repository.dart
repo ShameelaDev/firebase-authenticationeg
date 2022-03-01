2d3f571425491689ba7f11524d0d2c7dacc18d56
ShameelaDev <shemeelamoidheen@gmail.com>
2022-03-01 11:45:37 +0530

    authentication repository file created

--- lib/src/bloc/authentication/authentication_repository.dart
+++ lib/src/bloc/authentication/authentication_repository.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthRepository{
-
-  registerWitheEmailPassword(String email, String password)async{
-    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
-  return userCredential.user;
+class AuthRepository {
+  registerWitheEmailPassword(String email, String password) async {
+    UserCredential userCredential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return userCredential.user;
   }
 
-  loginWitheEmailPassword(String email, String password)async{
-    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+  loginWitheEmailPassword(String email, String password) async {
+    UserCredential userCredential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
     return userCredential.user;
   }
 }
