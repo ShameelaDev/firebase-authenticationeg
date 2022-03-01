a088b2dd86e272fd23a2d92afebf56c1d9963881
ShameelaDev <shemeelamoidheen@gmail.com>
2022-03-01 13:09:44 +0530

    authentication state and cubile file created

--- lib/src/bloc/authentication/authentication_cubit.dart
+++ lib/src/bloc/authentication/authentication_cubit.dart
@@ -1,8 +1,41 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_authentication/src/bloc/authentication/authentication_repository.dart';
 
 part 'authentication_state.dart';
 
 class AuthenticationCubit extends Cubit<AuthenticationState> {
-  AuthenticationCubit() : super(AuthenticationInitial());
+  final AuthRepository authRepository;
+  AuthenticationCubit(this.authRepository) : super(AuthenticationInitial());
+
+  registerWithEmailPassword(String email, String password) async {
+    emit(AuthenticationLoading());
+    try {
+      UserCredential userCredential =
+          await authRepository.registerWithEmailPassword(email, password);
+      if (userCredential == null) {
+        emit(AuthenticationError());
+      } else {
+        emit(AuthenticationSucess(userCredential));
+      }
+    } catch (ex) {
+      emit(AuthenticationError());
+    }
+  }
+
+  loginWithEmailPassword(String email, String password) async {
+    emit(AuthenticationLoading());
+    try {
+      UserCredential userCredential =
+          await authRepository.registerWithEmailPassword(email, password);
+      if (userCredential == null) {
+        emit(AuthenticationError());
+      } else {
+        emit(AuthenticationSucess(userCredential));
+      }
+    } catch (ex) {
+      emit(AuthenticationError());
+    }
+  }
 }
